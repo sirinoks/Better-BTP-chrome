0dfa45b498bf7e50e9d72b0c22e09afd0bc87aed
Sirinoks <shinigamoo@gmail.com>
2020-05-15 22:19:17 +0100

    dark theme mostly

--- center.js
+++ center.js
@@ -7,11 +7,29 @@ console.log(main);
 
 main.classList.add("center");
 
+//fix the main width
+main.removeAttribute("width");
+
+//footer
+const footer = document.getElementsByTagName("tbody")[0].lastElementChild;
+console.log("footer:");
+console.log(footer);
+
 //fix the header
 const header = document.getElementsByClassName("tbanner")[0];
 console.log("header");
 console.log(header);
 header.remove();
+
+//remove img/license
+const rightFooter = footer.getElementsByClassName("panel2")[0];
+
+while(rightFooter.lastElementChild) {
+    rightFooter.removeChild(rightFooter.lastElementChild);
+}
+
+//footer.getElementsByTagName("img")[0].remove();
+//footer.getElementsByTagName("a")[0].remove();
 
 //change the theme
 function darkTheme(toggle) {//how do I do dis???
